2f8821c7763ce999947f4d209d4f7ff373f7f2b8
Aayush Tyagi <tyagiaayush5@gmail.com>
2021-02-17 23:41:09 +0530

    LL OPERATION

--- Linked List/Linked_list_Operation.cpp
+++ Linked List/Linked_list_Operation.cpp
@@ -31,13 +31,53 @@ void insert(node **head, int n)
     }
 }
 
+void display(node *head)
+{
+    while(head!=NULL)
+    {
+        cout<<head->info<<" ";
+        head=head->next;
+    }
+}
+
+int Listlength(node *head)
+{
+    int c=0;
+    while(head!=NULL)
+    {
+        head=head->next;
+        c++;
+    }
+    return c;
+}
 int main()
 {
     node *head=NULL;
+    int choice;
     while(1)
     {
-        int n;
-        cin>>n;
-        insert(&head, n);
+        cout<<"\n1,2,3 Insert at begin end and mid"<<endl;
+        cout<<"4 display 5 length count"<<endl;
+        cin>>choice;
+        switch(choice)
+        {
+        case 1:
+            int n;
+            cin>>n;
+            insert(&head, n);
+            break;
+        case 2:
+            break;
+        case 3:
+            break;
+        case 4:
+            display(head);
+            break;
+        case 5:
+            cout<<Listlength(head)<<endl;
+            break;
+
+        }
+
     }
 }

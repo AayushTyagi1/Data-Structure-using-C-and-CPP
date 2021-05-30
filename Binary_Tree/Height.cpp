a1c7f25b4d64da384c08a67508dcc4aa5edbd9fd
Aayush Tyagi <tyagiaayush5@gmail.com>
2021-05-30 19:50:06 +0530

    Level Order Traversal BT

---
+++ Binary_Tree/Height.cpp
@@ -0,0 +1,65 @@
+
+#include <iostream>
+#include <queue>
+using namespace std;
+
+struct node
+{
+    int key;
+    node *left;
+    node *right;
+    node(int k)
+    {
+        key = k;
+        left = right = NULL;
+    }
+};
+
+int height(node *root)
+{
+    if (!root)
+        return 0;
+    return 1 + max(height(root->left), height(root->right));
+}
+void kdistance(node *root, int k)
+{
+    if (root == NULL)
+        return;
+    if (k == 0)
+    {
+        cout << root->key << " ";
+    }
+    kdistance(root->left, k - 1);
+    kdistance(root->right, k - 1);
+}
+void levelOrder(node *root)
+{
+    if (root == NULL)
+        return;
+    queue<node *> q;
+    q.push(root);
+    while (!q.empty())
+    {
+        node *t = q.front();
+        q.pop();
+        cout << t->key << " ";
+        if (t->left)
+            q.push(t->left);
+        if (t->right)
+            q.push(t->right);
+    }
+}
+
+int main()
+{
+    node *root = new node(10);
+    root->left = new node(20);
+    root->right = new node(30);
+    root->right->left = new node(40);
+    root->left->right = new node(50);
+    root->right->right = new node(70);
+    cout << height(root) << endl;
+    kdistance(root, 2);
+    cout << endl;
+    levelOrder(root);
+}

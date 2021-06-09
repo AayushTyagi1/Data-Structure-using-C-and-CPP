398e53014e2e35d854fdc20521060818b157b353
Aayush Tyagi <tyagiaayush5@gmail.com>
2021-06-09 12:02:38 +0530

    Binary Tree Search

---
+++ Binary_Search_Tree/1search.cpp
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node
+{
+    int val;
+    struct Node *left;
+    struct Node *right;
+    Node(int k)
+    {
+        val = k;
+        left = right = NULL;
+    }
+};
+
+bool search(Node *root, int n)
+{
+    if (root == NULL)
+        return false;
+    if (root->val == n)
+        return true;
+    if (root->val < n)
+        return search(root->right, n);
+    else
+        return search(root->left, n);
+}
+int main()
+{
+    Node *root = new Node(15);
+    root->left = new Node(5);
+    root->right = new Node(20);
+    root->right->left = new Node(18);
+    root->right->right = new Node(80);
+    root->left->left = new Node(3);
+    root->right->left->left = new Node(16);
+    cout << search(root, 16) << endl
+         << search(root, 19);
+}

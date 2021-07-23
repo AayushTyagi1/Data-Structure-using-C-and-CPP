aa4edbd09e0da08a3fb8d358ca9b6c6ddb153b05
Aayush Tyagi <tyagiaayush5@gmail.com>
2021-07-24 02:31:19 +0530

    PQueue minHEap

--- Heap/priority_queue.cpp
+++ Heap/priority_queue.cpp
@@ -12,4 +12,22 @@ int main()
         cout << p.top() << endl;
         p.pop();
     }
+    priority_queue<int, vector<int>, greater<int>> q;
+    q.push(10);
+    q.push(5);
+    q.push(20);
+    while (!q.empty())
+    {
+        cout << q.top() << " ";
+        q.pop();
+    }
+    vector<int> v = {10, 5, 7, 30, 4};
+    int arr[] = {9, 5, 2, 1, 6};
+    priority_queue<int, vector<int>, greater<int>> pq(arr, arr + 5);
+    priority_queue<int> newp(v.begin(), v.end());
+    while (!pq.empty())
+    {
+        cout << pq.top() << " ";
+        pq.pop();
+    }
 }

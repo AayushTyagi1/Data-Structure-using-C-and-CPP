97d34e2bdc63ad3d703b822f1b5556104fe93d2e
Aayush Tyagi <tyagiaayush5@gmail.com>
2021-07-25 18:33:47 +0530

    HEAP HARD PROBLEMS

--- Heap/mergeKsort.cpp
+++ Heap/mergeKsort.cpp
@@ -1,45 +1,65 @@
-class Solution
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Triplet
 {
-public:
-    //Function to merge k sorted arrays.
-    struct triplet
+    int val, aPos, vPos;
+    Triplet(int v, int ap, int vp)
     {
-        int val, ap, vp;
-        triplet(int v, int a, int b)
-        {
-            val = v;
-            ap = a;
-            vp = v;
-        }
-    };
-    struct mycmp
-    {
-        bool operator()(triplet &t1, triplet &t2)
-        {
-            return t1.val > t2.val;
-        }
-    };
-    vector<int> mergeKArrays(vector<vector<int>> arr, int K)
-    {
-        vector<int> res;
-        priority_queue<triplet, vector<triplet>, mycmp> pq;
-        for (int i = 0; i < arr.size(); i++)
-        {
-            triplet t(arr[i][0], i, 0);
-            pq.push(t);
-        }
-        while (!pq.empty())
-        {
-            triplet cur = pq.top();
-            pq.pop();
-            res.push_back(cur.val);
-            int apos = cur.ap, vpos = cur.vp;
-            if (arr[apos].size() > vpos + 1)
-            {
-                triplet t(arr[apos][vpos + 1], apos, vpos + 1);
-                pq.push(t);
-            }
-        }
-        return res;
+        val = v;
+        aPos = ap;
+        vPos = vp;
     }
 };
+
+struct MyComp
+{
+    bool operator()(Triplet &t1, Triplet &t2)
+    {
+        return t1.val > t2.val;
+    }
+};
+
+vector<int> mergeArr(vector<vector<int>> &arr)
+{
+    vector<int> res;
+
+    priority_queue<Triplet, vector<Triplet>, MyComp> pq;
+
+    for (int i = 0; i < arr.size(); i++)
+    {
+        Triplet t(arr[i][0], i, 0);
+        pq.push(t);
+    }
+
+    while (pq.empty() == false)
+    {
+        Triplet curr = pq.top();
+        pq.pop();
+        res.push_back(curr.val);
+        int ap = curr.aPos;
+        int vp = curr.vPos;
+        if (vp + 1 < arr[ap].size())
+        {
+            Triplet t(arr[ap][vp + 1], ap, vp + 1);
+            pq.push(t);
+        }
+    }
+
+    return res;
+}
+
+int main()
+{
+
+    vector<vector<int>> arr{{10, 20, 30},
+                            {5, 15},
+                            {1, 9, 11, 18}};
+
+    vector<int> res = mergeArr(arr);
+    cout << "Merged array is " << endl;
+    for (auto x : res)
+        cout << x << " ";
+
+    return 0;
+}

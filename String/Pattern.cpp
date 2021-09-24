e84b2045f753468c7a747ae652fe992790b55511
Aayush Tyagi <tyagiaayush5@gmail.com>
2021-09-24 21:49:05 +0530

    Pattern

---
+++ String/Pattern.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str, pat;
+    cin >> str >> pat;
+    vector<int> ind;
+    int j = 0;
+    for (int i = 0; i < str.length(); i++)
+    {
+        if (pat[j] == str[i])
+        {
+            int temp = i;
+            while (pat[j] && pat[j] == str[i])
+            {
+                i++;
+                j++;
+            }
+            if (j == pat.length())
+                ind.push_back(temp);
+            j = 0;
+            i = temp + 1;
+        }
+    }
+    for (int i = 0; i < ind.size(); i++)
+    {
+        cout << ind[i] << " ";
+    }
+}

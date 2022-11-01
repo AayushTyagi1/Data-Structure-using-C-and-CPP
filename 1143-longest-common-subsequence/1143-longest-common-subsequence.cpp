74f5e6d15b5bc8737b5522bdd339404533ac3b95
Aayush Tyagi <tyagiaayush5@gmail.com>
2022-11-02 01:38:00 +0530

    Time: 81 ms (11.79%), Space: 13.3 MB (12.17%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.cpp
@@ -1,18 +1,17 @@
 class Solution {
 public:
-    int dp[1005][10005];
-    int LCSHelper(string &s1, string &s2, int i, int j) {
+    int LCSHelper(string &s1, string &s2, int i, int j, vector<vector<int>> &dp) {
         if(i==s1.length() || j==s2.length())
             return 0;
         if(dp[i][j]!=-1)
             return dp[i][j];
         if(s1[i]==s2[j])
-            return dp[i][j] = 1+LCSHelper(s1, s2, i+1, j+1);
-        return dp[i][j]=max(LCSHelper(s1, s2, i+1, j),LCSHelper(s1, s2, i, j+1));
+            return dp[i][j] = 1+LCSHelper(s1, s2, i+1, j+1, dp);
+        return dp[i][j]=max(LCSHelper(s1, s2, i+1, j, dp),LCSHelper(s1, s2, i, j+1, dp));
     }
     int longestCommonSubsequence(string s1, string s2) {
         int m=s1.length(), n=s2.length();
-        memset(dp, -1, sizeof(dp));
-        return LCSHelper(s1, s2, 0, 0);
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        return LCSHelper(s1, s2, 0, 0, dp);
     }
 };

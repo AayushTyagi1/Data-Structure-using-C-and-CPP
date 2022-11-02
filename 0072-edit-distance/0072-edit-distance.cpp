26bf13409d2d47c7f91b73fb8bf712a451f6b08a
Aayush Tyagi <tyagiaayush5@gmail.com>
2022-11-03 01:28:49 +0530

    Time: 16 ms (76.82%), Space: 9 MB (60.07%) - LeetHub

--- 0072-edit-distance/0072-edit-distance.cpp
+++ 0072-edit-distance/0072-edit-distance.cpp
@@ -1,23 +1,23 @@
 class Solution {
 public:
-    int LCS(string &w1, string &w2, int i, int j, vector<vector<int>> &dp) {
-        if(i==w1.size() && j==w2.size())
-            return 0;
-        if(i==w1.size())
-            return w2.size()-j;
-        if(j==w2.size())
-            return w1.size()-i;
-        if(dp[i][j]!=-1)
-            return dp[i][j];
-        if(w1[i]==w2[j])
-            return dp[i][j] = LCS(w1, w2, i+1, j+1, dp);
-        int insert = 1 + LCS(w1, w2, i, j+1, dp);
-        int del = 1 + LCS(w1, w2, i+1, j, dp);
-        int rep = 1 + LCS(w1, w2, i+1, j+1, dp);
-        return dp[i][j] = min(insert, min(del, rep));
-    }
+    int LCS(string &w1, string &w2, int m, int n) {
+        vector<vector<int>> dp(m+1, vector<int>(n+1));
+       for(int i=0;i<=m;i++)
+       {
+           for(int j=0;j<=n;j++)
+           {
+               if(i==0)
+                   dp[i][j]=j;
+               else if(j==0)
+                   dp[i][j]=i;
+               else if(w1[i-1]==w2[j-1])
+                   dp[i][j]=dp[i-1][j-1];
+               else dp[i][j] = 1+min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j]));
+           }
+       }
+        return dp[m][n];
+    }    
     int minDistance(string word1, string word2) {
-        vector<vector<int>> dp(word1.size(), vector<int>(word2.size(), -1));
-        return LCS(word1, word2, 0, 0, dp);
+        return LCS(word1, word2, word1.size(), word2.size());
     }
 };

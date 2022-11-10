db5bd284863a2b8f0b51d6be5c7096c902864590
Aayush Tyagi <tyagiaayush5@gmail.com>
2022-11-10 17:27:00 +0530

    Time: 14 ms (97.70%), Space: 10.5 MB (81.99%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.cpp
@@ -1,6 +1,24 @@
 class Solution {
 public:
     string removeDuplicates(string s) {
+        //Simple Two pointer approach
+        //Time complexity O(N)
+        int i=0;
+        int l = 0, r = s.length();
+        while(i<r)
+        {
+            s[l] = s[i];
+            if (l > 0 && s[l - 1] == s[l])
+                l -= 2;
+            i++;l++;
+        }
+        return s.substr(0, l);
+    }
+};
+
+/*
+
+ string removeDuplicates(string s) {
         //First approach Delete
         //Time complexity O(N^2) as s.erase has O(N) complexity in itself
         int l = s.length()-1;
@@ -16,4 +34,5 @@ public:
         }
         return s;
     }
-};
+    
+*/

2a34fc868c933e11cf8a8f062dc4135071aa31ef
Aayush Tyagi <tyagiaayush5@gmail.com>
2022-11-15 01:33:16 +0530

    Time: 10 ms (96.69%), Space: 41.4 MB (87.67%) - LeetHub

--- 1114-print-in-order/1114-print-in-order.java
+++ 1114-print-in-order/1114-print-in-order.java
@@ -1,25 +1,43 @@
 class Foo {
-    private Semaphore secondSemaphore;
-    private Semaphore thirdSemaphore;
+    private Object lock1;
+    private Object lock2;
+    private boolean oneDone;
+    private boolean twoDone;
     
     public Foo() {
-        secondSemaphore = new Semaphore(0);
-        thirdSemaphore = new Semaphore(0);
+        lock1 = new Object();
+        lock2 = new Object();
+        oneDone = false;
+        twoDone = false;
     }
 
     public void first(Runnable printFirst) throws InterruptedException {
-        printFirst.run();
-        secondSemaphore.release();
+        synchronized (lock1) {
+            printFirst.run();
+            oneDone = true;
+            lock1.notifyAll();
+        }
     }
 
     public void second(Runnable printSecond) throws InterruptedException {
-        secondSemaphore.acquire();
-        printSecond.run();
-        thirdSemaphore.release();
+        synchronized (lock1) {
+            synchronized (lock2) {
+                while (!oneDone) {
+                    lock1.wait();
+                }
+                printSecond.run();
+                twoDone = true;
+                lock2.notifyAll();
+            }
+        }
     }
 
     public void third(Runnable printThird) throws InterruptedException {
-        thirdSemaphore.acquire();
-        printThird.run();
+        synchronized (lock2) {
+            while (!twoDone) {
+                lock2.wait();
+            }
+            printThird.run();
+        }
     }
 }
